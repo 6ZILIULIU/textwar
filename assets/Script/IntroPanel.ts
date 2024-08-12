9ca04d55587122864b828dc9bc8957f6353c6d13
tangc <291695377@qq.com>
2024-08-12 19:00:49 +0800

    1

--- assets/Script/IntroPanel.ts
+++ assets/Script/IntroPanel.ts
@@ -3,83 +3,73 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class MsgPanel extends cc.Component {
 
+    @property
+    playOnLoad = false;
+
     @property(cc.Node)
     infoBoard: cc.Node = null;
+    @property(cc.Node)
+    btnNext: cc.Node = null;
+    
 
-    @property(cc.Prefab)
-    infoPrefab: cc.Prefab = null;
+    @property
+    introFileName = ""
 
     infoLabelHeight = 30;
 
+    lastInfoLabel: cc.Label = null;
 
-
-    init() {
-        // TODO: init
-        this.clearInfoBoard();
-        this.waitForInfo();
-    }
-
-    clearInfoBoard() {
-        this.infoBoard.content.removeAllChildren();
-    }
-
-    waitForInfo() {
-        // 监听部队消息
-        cc.find('Canvas').on('echo', this.onReceiveInfo, this);
-    }
-
-    onReceiveInfo(infoStr: string, option?: any) {
-        this.displayInfo(infoStr,);
-    }
-
-    showNewestMsg() {
-        this.infoBoard.scrollToBottom(0.1);
-    }
-
-    displayInfo(infoStr: string) {
-        // let infoLabelNode = new cc.Node('info');
-        // infoLabelNode.setPosition(-this.infoBoard.node.width / 2 + 20, 0);
-        // infoLabelNode.setAnchorPoint(0, 0.5)
-        // let label = infoLabelNode.addComponent(cc.Label);
-        // label.fontSize = 26;
-        // label.horizontalAlign = cc.Label.HorizontalAlign.LEFT;
-        // label.verticalAlign = cc.Label.VerticalAlign.CENTER;
-        // label.overflow = cc.Label.Overflow.RESIZE_HEIGHT;
-        let infoLabelNode = cc.instantiate(this.infoPrefab);
-        let label = infoLabelNode.getComponent(cc.Label);
-        infoLabelNode.parent = this.infoBoard.content;
-        infoLabelNode.y = this.infoBoard.content.childrenCount * this.infoLabelHeight;
-        this.updateContentHeight();
-        // label.string = infoStr;
-        let str = infoStr;
-        // str = str.match(/.{1,30}/g).join('\n');
-        this.typeingEffect(label, str);
-        // this.showNewestMsg();
-    }
-    updateContentHeight() {
-        let childHeight = 0;
-        this.infoBoard.content.children.forEach(v => {
-            childHeight += v.height;
-        })
-        this.infoBoard.content.height = childHeight + this.infoBoard.node.height;
-    }
-
-    async typeingEffect(infoLabel: cc.Label, infoStr: string) {
-        for (let i = 0; i < infoStr.length; i++) {
-            await Utils.asyncWait(0.01);
-            infoLabel.string += infoStr[i];
-            // if (infoLabel.node.width)
+    protected start(): void {
+        if (this.playOnLoad) {
+            this.play();
         }
     }
 
-
-    isInfoExceeded() {
-        // TODO: check if info exceeded
-        return this.infoBoard.content.childrenCount >= 100;
+    async play() {
+        if (!this.introFileName) return;;
+        let introTxt = await Utils.loadAsset('IntroText', this.introFileName, cc.JsonAsset)
+        let txtlist = introTxt.text.split('\n')
+        for (let i = 0; i < txtlist.length; i++) {
+            await this.displayInfo(txtlist[i]);
+            await this.hideLastInfo()
+            await Utils.asyncWait(1);
+        }
     }
 
-    removeOldestInfo() {
-        // TODO: remove oldest info
-        this.infoBoard.content.children[0].removeFromParent();
+    async hideLastInfo() {
+        if (cc.isValid(this.lastInfoLabel)) {
+            await new Promise(resolve => {
+                this.lastInfoLabel.node.runAction(cc.sequence(cc.fadeTo(0.5, 0), cc.callFunc(() => {
+                    resolve(1);
+                })))
+            })
+        }
+    }
+
+    async displayInfo(infoStr: string) {
+        let infoLabelNode = new cc.Node('info');
+        infoLabelNode.setPosition(0, 0);
+        infoLabelNode.setAnchorPoint(0.5, 0.5)
+        let label = infoLabelNode.addComponent(cc.Label);
+        label.fontSize = 40;
+        label.horizontalAlign = cc.Label.HorizontalAlign.CENTER;
+        label.verticalAlign = cc.Label.VerticalAlign.CENTER;
+        label.overflow = cc.Label.Overflow.NONE;
+        infoLabelNode.parent = this.infoBoard;
+        infoLabelNode.color = cc.Color.WHITE;
+        label.string = infoStr;
+        label.node.opacity = 0;
+        this.lastInfoLabel = label;
+
+        await new Promise(resolve => {
+            this.lastInfoLabel.node.runAction(cc.sequence(
+                cc.fadeTo(0.5, 255),
+                cc.delayTime(2),
+                cc.callFunc(() => {
+                    resolve(1);
+                })
+            ))
+        })
+
     }
 }
